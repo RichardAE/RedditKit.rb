0a9fd16e5b636e03308933209e1b7bac4b9d2a57
RichardAE <richard@raelwebsolutions.com>
2014-07-21 21:28:52 +0100

    changes to support OAuth2

--- lib/redditkit/client.rb
+++ lib/redditkit/client.rb
@@ -43,30 +43,24 @@ module RedditKit
 
     attr_reader :username
     attr_reader :current_user
-    attr_reader :cookie
-    attr_reader :modhash
 
     attr_accessor :api_endpoint
     attr_accessor :authentication_endpoint
     attr_accessor :user_agent
     attr_accessor :middleware
+    attr_accessor :token
 
-    def initialize(username = nil, password = nil)
-      @username = username
-      @password = password
-
-      @cookie = nil
-      @modhash = nil
-
-      sign_in(username, password) unless username.nil? || password.nil?
+    def initialize(user = nil)
+      @username = user.name
+      @token = user.token
     end
 
     def api_endpoint
-      @api_endpoint ||= 'http://www.reddit.com/'
+      @api_endpoint ||= 'https://oauth.reddit.com/'
     end
 
     def authentication_endpoint
-      @authentication_endpoint ||= 'https://ssl.reddit.com/'
+      @authentication_endpoint ||= 'https://oauth.reddit.com/'
     end
 
     def user_agent
@@ -109,18 +103,15 @@ module RedditKit
         request = authenticated_request_configuration(method, path, parameters)
         request_connection.send(method.to_sym, path, parameters, &request).env
       else
-        request_connection.send(method.to_sym, path, parameters).env
+        raise RedditKit::RequestError
       end
     rescue Faraday::Error::ClientError
       raise RedditKit::RequestError
     end
 
     def authenticated_request_configuration(method, path, parameters)
-      fail RedditKit::NotAuthenticated unless signed_in?
-
       proc do |request|
-        request.headers['Cookie'] = "reddit_session=#{@cookie}"
-        request.headers['X-Modhash'] = @modhash
+        request.headers['Authorization'] = "bearer #{@token}"
       end
     end
 
